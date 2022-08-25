aa8c834b3269cbcca055525ba91d5058858beb72
user <user@host021-ubuntu-1804.lxd>
2022-08-25 03:13:52 +0000

    resubmit no change

--- resource/ecpt/build/obj/src/boot1.d
+++
@@ -1,2 +0,0 @@
-build/obj/src/boot1.o: src/boot1.S src/restore_rom_addr.h src/encoding.h \
- src/csr.h
--- resource/ecpt/build/obj/src/boot2.d
+++
@@ -1,2 +0,0 @@
-build/obj/src/boot2.o: src/boot2.S src/restore_rom_addr.h src/encoding.h \
- src/csr.h
